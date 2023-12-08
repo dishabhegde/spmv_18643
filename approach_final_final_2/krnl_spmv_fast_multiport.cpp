0647bebb57fd71bc16c7e1dd281102135c1ff80e
dishabhegde <dishabhegde@gmail.com>
2023-12-08 01:22:06 -0500

    added 1 stream version

---
+++ approach_final_final_2/krnl_spmv_fast_multiport.cpp
@@ -0,0 +1,219 @@
+#include "spmv.h"
+#include <hls_stream.h>
+
+#define II 1
+#define NUM_STREAMS 1
+
+extern "C" {
+void spmv_kernel(
+		volatile int rows_length[NUM_ROWS],
+		volatile int rows_length_pad[NUM_ROWS],
+		int cols[NNZ],
+		DTYPE values[NNZ],
+		DTYPE y[SIZE],
+		DTYPE x_dup[SIZE],
+		int cnt[1])
+{
+
+
+
+#pragma HLS ARRAY_PARTITION variable=rows_length_pad dim=1
+#pragma HLS ARRAY_PARTITION variable=rows_length dim=1
+#pragma HLS ARRAY_PARTITION variable=x_dup dim=1
+
+
+	int row_length_pad = 0, row_length = 0, row_counter = 0;
+
+//#pragma HLS ARRAY_PARTITION variable=row_length_pad dim=1
+//#pragma HLS ARRAY_PARTITION variable=row_length dim=1
+//#pragma HLS ARRAY_PARTITION variable=row_counter dim=1
+
+	hls::stream<DTYPE> values_fifo;
+	hls::stream<int>   cols_fifo;
+	hls::stream<DTYPE> results_fifo;
+
+#pragma HLS stream variable=values_fifo type=fifo depth=32
+//#pragma HLS stream variable=values_fifo[1] type=fifo depth=32
+//#pragma HLS stream variable=values_fifo[2] type=fifo depth=32
+//#pragma HLS stream variable=values_fifo[3] type=fifo depth=32
+//#pragma HLS stream variable=values_fifo[4] type=fifo depth=32
+//#pragma HLS stream variable=values_fifo[5] type=fifo depth=32
+//#pragma HLS stream variable=values_fifo[6] type=fifo depth=32
+//#pragma HLS stream variable=values_fifo[7] type=fifo depth=32
+#pragma HLS stream variable=cols_fifo type=fifo depth=32
+//#pragma HLS stream variable=cols_fifo[1] type=fifo depth=32
+//#pragma HLS stream variable=cols_fifo[2] type=fifo depth=32
+//#pragma HLS stream variable=cols_fifo[3] type=fifo depth=32
+//#pragma HLS stream variable=cols_fifo[4] type=fifo depth=32
+//#pragma HLS stream variable=cols_fifo[5] type=fifo depth=32
+//#pragma HLS stream variable=cols_fifo[6] type=fifo depth=32
+//#pragma HLS stream variable=cols_fifo[7] type=fifo depth=32
+#pragma HLS stream variable=results_fifo type=fifo depth=32
+//#pragma HLS stream variable=results_fifo[1] type=fifo depth=32
+//#pragma HLS stream variable=results_fifo[2] type=fifo depth=32
+//#pragma HLS stream variable=results_fifo[3] type=fifo depth=32
+//#pragma HLS stream variable=results_fifo[4] type=fifo depth=32
+//#pragma HLS stream variable=results_fifo[5] type=fifo depth=32
+//#pragma HLS stream variable=results_fifo[6] type=fifo depth=32
+//#pragma HLS stream variable=results_fifo[7] type=fifo depth=32
+
+
+//#pragma HLS INTERFACE mode=axis port=values_fifo[0]
+//#pragma HLS INTERFACE mode=axis port=values_fifo[1]
+//#pragma HLS INTERFACE mode=axis port=cols_fifo[0]
+//#pragma HLS INTERFACE mode=axis port=cols_fifo[1]
+//#pragma HLS INTERFACE mode=axis port=results_fifo[0]
+//#pragma HLS INTERFACE mode=axis port=results_fifo[1]
+
+
+	DTYPE sum = 0;
+	DTYPE value;
+	int col;
+	DTYPE term[II];
+	int new_nnz_split = 0;
+	int nnz_split = 0;
+
+//#pragma HLS ARRAY_PARTITION variable=value dim=1
+//#pragma HLS ARRAY_PARTITION variable=col dim=1
+//#pragma HLS ARRAY_PARTITION variable=term dim=1
+//#pragma HLS ARRAY_PARTITION variable=sum dim=1
+//#pragma HLS ARRAY_PARTITION variable=new_nnz_split dim=1
+//#pragma HLS ARRAY_PARTITION variable=nnz_split dim=1
+
+
+	int index = 0;
+		for (int l = 0; l < NUM_ROWS; l++) {
+#pragma pipeline off
+			new_nnz_split += rows_length_pad[l];
+			nnz_split += rows_length[l];
+		}
+
+		{
+#pragma HLS DATAFLOW
+			for(int j = 0; j < new_nnz_split; j++) {
+#pragma HLS pipeline off
+				if (j < nnz_split) {
+					values_fifo << values[index];
+					cols_fifo << cols[index++];
+				}
+			}
+
+//	for (int i = 0; i < NNZ; i++) {
+//#pragma HLS PIPELINE
+//		values_fifo << values[i];
+//		cols_fifo   << cols[i];
+//	}
+		Loop_rows: for (int i = 0; i < NUM_ROWS; i++) {
+			Loop_onerow: for (int n = 0; n < rows_length_pad[i]; n+=II) {
+
+				#pragma HLS PIPELINE
+				if (row_length_pad == 0) {
+					row_length_pad = rows_length_pad[i];
+					row_length = rows_length[i];
+					row_counter = 0;
+					sum = 0;
+				}
+
+				Loop_II_sec: for (int j = 0; j < II; j++) {
+					row_counter++;
+					if (row_counter > row_length) {
+						term[j] = 0;
+					} else {
+						value = values_fifo.read();
+						col   = cols_fifo.read();
+						term[j] = value * x_dup[col];
+					}
+				}
+
+				DTYPE sum_tmp = 0;
+				for (int j = 0; j < II; j++) {
+					sum_tmp += term[j];
+				}
+				sum += sum_tmp;
+
+				row_length_pad -= II;
+				if (row_length_pad == 0) {
+					results_fifo << sum;
+				}
+			}
+		}
+
+
+		for (int i = 0; i < NUM_ROWS; i++) {
+	#pragma HLS PIPELINE off
+			y[i] = results_fifo.read();
+		}
+		}
+	cnt[0]++;
+}
+void krnl_spmv_fast_multiport(
+		int cols[NNZ],
+		DTYPE values[NNZ],
+		DTYPE x[SIZE],
+		int rows_length_in[NUM_ROWS],
+		int rows_length_pad_in[NUM_ROWS],
+		DTYPE y[SIZE])
+{
+
+//#pragma HLS INTERFACE mode=bram port=rows_length
+//#pragma HLS INTERFACE mode=bram port=cols
+//#pragma HLS INTERFACE mode=bram port=values
+//#pragma HLS INTERFACE mode=bram port=rows_length_pad
+
+	// rowPtr to rows_length
+	volatile int rows_length[NUM_ROWS];
+//	int rows_length_int[NUM_ROWS] = {0};
+//	for (int i = 1; i < NUM_ROWS + 1; i++) {
+//#pragma HLS PIPELINE off
+//		rows_length[i - 1] = rowPtr[i] - rowPtr[i - 1];
+////		rows_length_int[i-1] = rowPtr[i] - rowPtr[i-1];
+//	}
+//
+	volatile int rows_length_pad[NUM_ROWS];
+
+#pragma HLS ARRAY_PARTITION variable=rows_length_pad dim=1
+#pragma HLS ARRAY_PARTITION variable=rows_length dim=1
+//
+//
+//	int new_nnz = 0;
+//	for (int i = 0; i < NUM_ROWS; i++) {
+//#pragma HLS PIPELINE
+//		int r = rows_length[i];
+//		int r_diff = r % II;
+//		if (r == 0) {
+//			rows_length_pad[i] = II;
+//			new_nnz += II;
+//		} else if (r_diff != 0) {
+//			rows_length_pad[i] = r + (II - r_diff);
+//			new_nnz += r + (II - r_diff);
+//		} else {
+//			rows_length_pad[i] = r;
+//			new_nnz += r;
+//		}
+//	}
+
+	for (int i = 0; i < NUM_ROWS; i++) {
+		rows_length[i] = rows_length_in[i];
+		rows_length_pad[i] = rows_length_pad_in[i];
+
+	}
+//	DTYPE x_dup[NUM_STREAMS][SIZE];
+//
+//#pragma HLS ARRAY_PARTITION variable=x_dup dim=1
+//	for (int i = 0; i < NUM_STREAMS; i++) {
+//		for (int k=0; k < SIZE; k++) {
+//
+//			x_dup[i][k] = x[k];
+//		}
+//	}
+
+	int cnt[1] = {0};
+	for(int i = 0; i < 100000; i++){
+#pragma HLS pipeline off
+#pragma HLS unroll off
+		spmv_kernel(rows_length, rows_length_pad, cols, values, y, x, cnt);
+	}
+	y[0] += cnt[0];
+
+}
+}
